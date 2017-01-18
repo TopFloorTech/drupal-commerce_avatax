248b6033667df98e73b595152ece2e520ba7db16
Ben McClure <ben.mcclure@gmail.com>
2017-01-17 22:45:27 -0600

    Initial commit (not ready)

---
+++ README.md
@@ -0,0 +1,9 @@
+ABOUT
+=====
+
+A generic plugin system for managing tax services in Drupal Commerce 2.x.
+
+DEPENDENCIES
+============
+
+- Drupal Commerce 2.x
---
+++ src/Plugin/Commerce/TaxService/AvaTax.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\commerce_avatax\Plugin\Commerce\TaxService;
+
+use Drupal\commerce_tax_service\Plugin\Commerce\TaxService\TaxServiceBase;
+
+class AvaTax extends TaxServiceBase {
+
+}
